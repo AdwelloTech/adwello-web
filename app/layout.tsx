8b183425a12f358f2ebd497d87fee0ccbe99b425
Farhat <mfarhat334@gmail.com>
2025-07-21 14:40:30 +0530

    add: meta tag for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,29 @@ export default function RootLayout({
           name="facebook-domain-verification"
           content="rv11va8jdu6lo2gbve0c5jzqe0u15t"
         />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1450111643000274');
+fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1450111643000274&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body
         className={clsx(
